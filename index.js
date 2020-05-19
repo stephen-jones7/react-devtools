348a4b3fe368afd129a5ace9f1bc71308a19372b
stephen-jones7 <stephen.jones7@protonmail.com>
2020-05-19 10:09:45 -0500

    2 files modified, 4 added, 2 deleted

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/components/App.jsx";
+import './styles.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
